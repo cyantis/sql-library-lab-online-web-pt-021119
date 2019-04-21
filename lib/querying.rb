9b776b426bea9ed289956a21c07093db702d144a
Cody Yantis <cyantis@gmail.com>
2019-04-21 23:33:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ end
 
 def select_series_title_with_most_human_characters
   "
-  SELECT series.title, characters.species FROM series
+  SELECT series.title, SUM(characters.species) FROM series
   JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
   "
