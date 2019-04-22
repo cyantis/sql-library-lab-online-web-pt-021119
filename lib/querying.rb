6e1cc2679b5614fb14eef807fe3b89aad8dc1d50
Cody Yantis <cyantis@gmail.com>
2019-04-22 00:21:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,6 +45,5 @@ def select_character_names_and_number_of_books_they_are_in
   "
   SELECT characters.name, character_books.book_id, COUNT(*) FROM characters
   JOIN character_books ON characters.id = character_books.character_id;
-
   "
 end
